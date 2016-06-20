7ddfd975fc974f145aa3bac73b6589a4de89d0f9
tabby336 <ioana.bogdan25@gmail.com>
2016-06-20 19:46:27 +0300

    finished help command

--- scr/models/command_executer/help_executer/help_handler.js
+++ scr/models/command_executer/help_executer/help_handler.js
@@ -4,9 +4,6 @@ var youtube = require('../youtube_executer/youtube_command_handlers');
 var jokes = require('../jokes_executer/jokes_command_handler');
 var google_mail = require('../gmail_executer/gmail_command_handlers');
 var google_calendar = require('../google_executer/calendar_command_handlers');
-/*
-var google_calendar = 
-*/
 
 var toClient = require('../../../controllers/send_to_client');
 
@@ -17,13 +14,15 @@ exports.help = function(req, res, cmd) {
 	console.log(splitted_help[0]);
 
 	if(splitted_help.length > 2) {
+		console.log("Sunt in > 2");
 		toClient.send(req, res, "NOT OK");
 		return;
 	}
 
 	var help = '';
 
-	if(splitted_help.lenth == 1) {
+	if(splitted_help.length == 1) {
+		console.log("Sunt aici");
 		help = help + facebook.help();
 		help = help + twitter.help();
 		help = help + youtube.help();
@@ -34,21 +33,30 @@ exports.help = function(req, res, cmd) {
 		return;
 	}	
 
-	switch(splitted_help[1]) {
+	console.log(splitted_help[1]);
+
+	switch(splitted_help[1].trim()) {
 		case "facebook":
 			help = help + facebook.help();
+			break;
 		case "twitter":
 			help = help + twitter.help();
+			break;
 		case "youtube":
 			help = help + youtube.help();
+			break;
 		case "jokes":
 			help = help + jokes.help();
+			break;
 		case "gmail":
 			help = help + google_mail.help();
+			break;
 		case "calendar":
 			help = help + google_calendar.help();
+			break;
 		default: 
-			toClient.send(req, res, "Not a provider");
+			help = "Not a provider";
+			break;
 	}
 
 	toClient.send(req, res, help);
